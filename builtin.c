acbf3be54fedda9c774cecec9eaa3c3b72029a09
sharifhabiman <hdidiersharif@gmail.com>
2021-11-04 11:04:40 +0200

    simple shell

---
+++ builtin.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+/**
+**shell_exit - exits the shell
+**Return: void
+**/
+
+	int shell_exit(void)
+	{
+		return (-1);
+	}
+
+
+/**
+**shell_env - prints environment
+**Return: void
+**/
+
+	int shell_env(void)
+	{
+		unsigned int i;
+
+		i = 0;
+		while (environ[i] != NULL)
+		{
+			write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+			write(STDOUT_FILENO, "\n", 1);
+			i++;
+		}
+		return (0);
+	}
